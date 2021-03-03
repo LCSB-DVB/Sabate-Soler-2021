809b6cc9e0b49c224f4f7c042b63621f13169ff1
MatthieuGobin <51372712+MatthieuGobin@users.noreply.github.com>
2021-03-03 15:07:21 +0100

    Add files via upload

---
+++ Marker analysis/Matlab/FINAL_MainOrganoidsOperettaSlide_FOXA2_MAP2_IBA1.m
@@ -0,0 +1,186 @@
+%% User inputs
+clear
+clc
+
+SetupMode = 0; % 1 for creating numeric organoid labels OR 0 for linking the final analysis to human labels
+SlideLayout = 'SSS_MGP2_AbC1_MAP2_647_FOXA2_568_IBA1_488.txt';
+
+SavePath = 'S:\HCS_Platform\Data\SoniaSabate\Operetta\Paper Microglia\FINAL ANALYSIS\ABC1P2';
+mkdir(SavePath)
+PreviewSavePath = 'S:\HCS_Platform\Data\SoniaSabate\Operetta\Paper Microglia\FINAL ANALYSIS\ABC1P2';
+PreviewSavePath = [SavePath, filesep, 'Previews'];
+mkdir(PreviewSavePath)
+
+% Creating a copy of the Main script that is running (the copyfile fucntion only works if you run the script not if you F9)
+FileNameShort = mfilename;
+newbackup = sprintf('%s_log.m',[SavePath, '\', FileNameShort]);
+FileNameAndLocation = [mfilename('fullpath')];
+currentfile = strcat(FileNameAndLocation, '.m');
+copyfile(currentfile,newbackup); 
+% Creating a file with the version that is running of Matlab
+Version = version();
+save([SavePath filesep 'MatlabVersion.mat'], 'Version')
+% Triggering fucntion of Dependencies -> This creates a folder having a copy of all the custom functions present in all the layers of the script
+f_LogDependencies(FileNameShort, SavePath); 
+%% Parallel pool control
+delete(gcp('nocreate'))
+myCluster = parcluster;
+Workers = myCluster.NumWorkers;
+%parpool(28) %for HURRICANE
+parpool(Workers) % for MEGATRON
+
+%% Run mode control
+if SetupMode
+    RunMode = 0;
+else
+    RunMode = 1;
+end
+
+%% Common part
+
+    channelID = 1; % channel to use for overview
+   
+InfoTable = readtable ('S:\Operetta\OperettaDB_LCSB\SSS_20191203_MGP2_AbC1_MAP2_647_FOXA2_568_IBA1_488\a95588a3-e741-4b48-b0f8-3827e9689d1b\metadata.csv');   
+ChannelNames = unique(InfoTable.Channel);
+    Channels = length(unique(InfoTable.Channel));
+    Planes = unique(InfoTable.Plane)';
+    Timepoints = unique(InfoTable.Timepoint)' + 1;
+    [GroupsTable, GroupsIm5DCellArray] = FindGroups(InfoTable); % it(GroupsTable)
+    
+%% Setup mode
+
+if SetupMode == 1
+    
+    Preview = CreateLabelHelpPreview(GroupsTable, PreviewSavePath);
+    imwrite(Preview, [PreviewSavePath, filesep, 'layout.png'])
+    Message = ['The plate Layout has been saved at ', [PreviewSavePath, filesep, 'layout.png'], '. Please save a text file without header, using tab separation, where the first column is the index number as shown in the preview and the second is the area name. Save the text file as SlideLayout_Date.txt in your working directory and set the variable SetupMode to 0 >>> Run'];
+    h = msgbox(Message);
+    
+else    
+%% Analysis mode
+    
+    % Load annotations
+    Layout = readtable(SlideLayout)
+    Layout.Properties.VariableNames = {'Idx', 'AreaName'};
+    
+    % Load images and organize in an XYC array
+    Groups = unique(GroupsTable(GroupsTable > 0))';
+    GroupPreviews = {};
+    ObjectsAll = {};
+ 
+%     %just 1st organoid
+%   for g =[10] 
+%  for a group of sections, use colon (:) --> example g=[1:15]
+
+    for g =[1:13] % Number of organoids
+     
+    XYMosaicCells = {};
+        GroupZone = GroupsTable == g;
+        [GroupIdxRowVec, GroupIdxColVec] = find(GroupZone); % linear indexes
+        Elements = sum(GroupZone(:));
+        InfoTablesThisGroup = {};
+        for e = 1:Elements % Fields of a given organoid
+            for c = 1:Channels
+            InfoTableThisField = GroupsIm5DCellArray{GroupIdxRowVec(e), GroupIdxColVec(e)};
+            InfoTablesThisGroup{e} = InfoTableThisField;
+            InfoTableThisChannel = InfoTableThisField(strcmp(InfoTableThisField.Channel, ChannelNames{c}), :);
+                clear Im4D
+                for t = Timepoints
+                    for p = Planes
+                        InfoTableThisChannelThisPlane = InfoTableThisChannel(InfoTableThisChannel.Plane == p, :);
+                        ImPathThisPlane = InfoTableThisChannelThisPlane.Path{:};   
+                        Im4D(:,:,t,p) = imread(ImPathThisPlane); % it(Im4D(:,:,t,p))
+                    end
+                end
+               XYMosaicCells{c}{GroupIdxRowVec(e), GroupIdxColVec(e)} = Im4D; % Operetta counterpart of XYmosaicCells for Opera
+            end
+        end
+
+        InfoTableThisGroup = vertcat(InfoTablesThisGroup{:});
+
+        %% Remove empty cells
+        XYMosaicCells = cellfun(@(x) GroupClipper(x),  XYMosaicCells, 'UniformOutput', false);
+
+        %% Stitch
+        XYmosaicContourCell = cellfun(@(x) stdfilt(x, ones(3)), XYMosaicCells{1}, 'UniformOutput', false);
+        XPositions = unique(InfoTableThisGroup.PositionX); % m
+        YPositions = unique(InfoTableThisGroup.PositionY); % m
+        ResolutionXY = 675 / 1360; % um per pixel
+        MaxPixelDrift = 30;
+        PreviewChannel = 1;
+        ShowProgress = 0;
+        [CroppedMosaic, StitchedIm] = f_stitching_operetta(XYMosaicCells, XYmosaicContourCell, XPositions, YPositions, ResolutionXY, MaxPixelDrift, PreviewChannel, ShowProgress);
+        GroupPreviews{g} = max(CroppedMosaic{channelID},[],3); %it(GroupPreviews{g})
+        
+        %% Image analysis
+        Label = Layout(g,:);
+         try
+            ObjectsThisOrganoid = New_f_ImageAnalysis_IBA1FOXA2MAP2(Label, CroppedMosaic{1}, CroppedMosaic{2}, CroppedMosaic{3}, CroppedMosaic{4},ChannelNames, PreviewSavePath);
+
+        catch
+             Errors{g} = 'Image analysis failed';
+             continue % next group g
+         end
+            ObjectsAll{g} = ObjectsThisOrganoid;
+
+    end
+%   Objects = Objects(Objects.NucMaskSum > 5000, :);
+   Objects = vertcat(ObjectsAll{:});
+   Objects = Objects(Objects.MAP2MaskSumSum > 150000, :);
+   Objects = Objects(Objects.FOXA2MaskSum > 150000, :);
+   
+    save([SavePath, filesep, 'Objects.mat'], 'Objects');
+    writetable(Objects, [SavePath, filesep, 'Objects.csv'])
+    writetable(Objects, [SavePath, filesep, 'Objects.xlsx']) 
+
+
+       
+%% Preview of the whole slide
+
+    SizeSingleIm = size(XYMosaicCells{1}{1,1});
+    SizeSingleIm = SizeSingleIm(1:2);
+    RescanGridSize = size(GroupsTable);
+    GreatPreview = zeros(SizeSingleIm(1)*RescanGridSize(1), SizeSingleIm(2)*RescanGridSize(2), 'uint16');
+    ImHeight = SizeSingleIm(1);
+    ImWidth = SizeSingleIm(2);
+    StartRCell = {};
+    StartCCell = {};
+
+  for g =[1:13]
+        g
+        StitchedGroupSize = size(GroupPreviews{g});
+        ZoneNow = GroupsTable == g;
+        [R,C] = find(ZoneNow)
+        StartR = min(R);
+        StartC = min(C);
+        StartRPixel = ((StartR-1) * ImHeight) + 1;
+        %EndRPixel = StartRPixel + (3 * ImHeight) - 1;
+        EndRPixel = StartRPixel + StitchedGroupSize(1) - 1;
+        StartCPixel = ((StartC-1) * ImWidth) + 1;
+        %EndCPixel = StartCPixel + (3 * ImWidth) - 1;
+        EndCPixel = StartCPixel + StitchedGroupSize(2) - 1;
+        GreatPreview(StartRPixel:EndRPixel, StartCPixel:EndCPixel) = GroupPreviews{g};
+        StartRCell{g} = StartRPixel;
+        StartCCell{g} = StartCPixel;
+    end
+
+    Zoomfactor = 50;
+    %GreatPreviewResized = imresize(imadjust(GreatPreview), 1/Zoomfactor);
+    GreatPreviewResized = imresize(imadjust(GreatPreview, [0 0.02], [0 1]), 1/Zoomfactor);
+
+  for g =[1:13]
+        GreatPreviewResized = insertText(GreatPreviewResized, [round(StartCCell{g}/Zoomfactor), round(StartRCell{g}/Zoomfactor)], num2str(g), 'FontSize', 12, 'BoxColor', 'red', 'TextColor', 'white');
+    end
+    
+    %imtool(GreatPreview)
+    %imtool(GreatPreviewResized)
+    imwrite(GreatPreviewResized, [SavePath, filesep, 'GreatPreview.png'])
+    
+    % it(GreatPreviewResized)
+    % save([SavePath, filesep, 'WorkspaceIncludingObjects.mat'])
+    
+end
+
+
+
+
---
+++ Marker analysis/Matlab/New_f_ImageAnalysis_IBA1FOXA2MAP2.m
@@ -0,0 +1,154 @@
+%function  [ObjectsThisOrganoid] = FINAL_f_ImageAnalysisPerOperettaOrganoid_cell_count(Label, ch1, ch2, ch3, ch4, ChannelNames, PreviewPath);
+function  [ObjectsThisOrganoid] = New_f_ImageAnalysis_IBA1FOXA2MAP2(Label, ch1, ch2, ch3, ch4, ChannelNames, PreviewPath);
+
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+    % vol(ch1, 0, 3000) % Alexa 488 >>> IBA1
+    % vol(ch2, 0, 1000) % Alexa 647 >>> MAP2
+    % vol(ch3, 0, 3000) % HOECHST 33342 >>> Hoechst imtool(max(ch2, [], 3))
+    % vol(ch4, 0, 500) % TRITC >>> FOXA2
+
+    %% Initialize variables
+    NucleiMask = [];
+    MAP2Mask = [];
+    FOXA2Mask = [];
+    IBA1Mask = []; 
+    
+    %% Segment nuclei
+    %vol(ch3, 0, 2000)
+    ch3BlurSmall = imfilter(double(ch3), fspecial('gaussian', 21, 1), 'symmetric');%vol(ch3BlurSmall)
+    ch3BlurBig = imfilter(double(ch3), fspecial('gaussian', 21, 3), 'symmetric');%vol(ch3BlurBig) %%kind of flatfield corraction, to account for different bk in the pic
+    ch3DoG = ch3BlurSmall - ch3BlurBig; %vol(ch3DoG, 0, 500, 'hot')
+    NucleiMask = ch3DoG > 40; %vol(NucleiMask)
+    NucleiMask = bwareaopen(NucleiMask, 200);%vol(NucleiMask)
+    %bwareaopen excludes from the mask what is smaller than the specified
+    %number of pixels
+    ch3LP = imfilter(ch3, fspecial('gaussian', 11, 1), 'symmetric');%vol(ch3LP, 0, 10000, 'hot')
+    NucMaskHigh =  (ch3LP > 2000) .* NucleiMask; %vol(NucMaskHigh, 0, 1) %%before3500 
+    % NucMaskHigh discards what is dead
+    NucMaskAlive = NucleiMask & ~NucMaskHigh; % vol(NucMaskAlive)
+     
+    %%  MAP2 (ch2)
+  
+    ch2MedFilt = []; 
+    SizeZ = size(ch2, 3);
+    parfor p = 1:SizeZ
+        ch2MedFilt(:,:,p) = medfilt2(ch2(:,:,p));
+    end
+    %vol(ch2MedFilt, 0, 1000, 'hot')
+     MAP2Mask = ch2MedFilt > 100; % vol(MAP2Mask)
+    MAP2DoG = imfilter(ch2, fspecial('gaussian', 11, 1), 'symmetric') - imfilter(ch2, fspecial('gaussian', 31, 10), 'symmetric');
+    %vol(MAP2DoG, 0, 20, 'hot')
+    MAP2DoGMask = MAP2DoG > 10;
+    %vol(MAP2DoGMask)
+    MAP2Mask = MAP2Mask & MAP2DoGMask;
+    %vol(MAP2Mask, 0, 1)
+    %it(max(MAP2Mask, [], 3))
+    %MAP2Mask = MAP2Mask & ~NucleiMask; %%%%%% IF THIS IS ON WILL CREATE AN ERROR IN THE %TUJ1
+    MAP2Mask = bwareaopen(MAP2Mask, 200);
+    %vol(MAP2Mask)
+    
+
+%% FOXA2 (ch4)
+ 
+    ch4BlurSmall = imfilter(double(ch4), fspecial('gaussian', 21, 1), 'symmetric');%vol(ch4BlurSmall)
+    ch4BlurBig = imfilter(double(ch4), fspecial('gaussian', 21, 3), 'symmetric');%vol(ch4BlurBig) %%kind of flatfield corraction, to account for different bk in the pic
+    ch4DoG = ch4BlurSmall - ch4BlurBig; %vol(ch4DoG, 0, 50, 'hot')
+    FOXA2Mask = ch4DoG > 15; %vol(FOXA2Mask)
+    FOXA2Mask = bwareaopen(FOXA2Mask, 500);%vol(FOXA2Mask)
+    %bwareaopen excludes from the mask what is smaller than the specified
+    %number of pixels
+    
+  
+   %%  IBA1 (ch1)
+   
+   ch1MedFilt = []; 
+    SizeZ = size(ch1, 3);
+    parfor p = 1:SizeZ
+        ch1MedFilt(:,:,p) = medfilt2(ch1(:,:,p));
+    end
+    %vol(ch1MedFilt, 0, 3000, 'hot')
+    IBA1Mask = ch1MedFilt > 40; % vol(IBA1Mask)
+    IBA1DoG = imfilter(ch1, fspecial('gaussian', 75, 5) - fspecial('gaussian', 75, 25), 'symmetric');
+    %IBA1DoG = gather(imfilter(gpuArray(ch1), gpuArray(fspecial('gaussian', 75, 5) - fspecial('gaussian', 75, 25)), 'symmetric'));
+    %vol(IBA1DoG, 0, 50, 'hot')
+    IBA1DoGMask = IBA1DoG > 50;
+    %vol(IBA1DoGMask)
+    
+    IBATopHat = imfilter(imtophat(ch1, strel('disk', 15)), fspecial('gaussian', 11, 5), 'symmetric');%vol(IBATopHat, 0, 500)
+    IBA1TopHatMask = IBATopHat > 150; % vol(IBA1TopHatMask)
+    
+    IBAStd = medfilt3(stdfilt(ch1, ones(3)));% vol(IBAStd, 0, 100)
+    IBAStdMask = IBAStd > 50;
+    IBAStdMask = bwareaopen(IBAStdMask,1000); %vol(IBAStdMask)
+   
+   
+    IBA1Mask = imreconstruct(IBAStdMask,IBA1Mask & IBA1DoGMask & IBA1TopHatMask);
+    %vol(IBA1Mask, 0, 1)
+    %it(max(IBA1Mask, [], 3))
+    %IBA1Mask = IBA1Mask & ~NucleiMask; %%%%%% IF THIS IS ON WILL CREATE ERROR IN THE %IBA1
+    IBA1Mask = bwareaopen(IBA1Mask, 1000);
+    %vol(IBA1Mask))
+
+  
+    %% Previews with mask
+    
+% Scalebar
+    imSize = [size(ch1, 1), size(ch1, 2)];
+    [BarMask, BarCenter] = f_barMask(200, 0.42, imSize, imSize(1)-200, 200, 25);
+    %it(BarMask)
+    
+    PreviewHoechst = imoverlay2(imadjust(max(ch3,[],3),[0 0.075]), bwperim(max(NucleiMask,[],3)), [1 0 0]);
+    PreviewHoechst = imoverlay2(PreviewHoechst, BarMask, [1 1 1]);
+    % imtool(PreviewHoechst)
+    
+    PreviewNucMaskAlive = imoverlay2(imadjust(max(ch3,[],3),[0 0.07]), bwperim(max(NucMaskAlive,[],3)), [1 0 0]);
+    PreviewNucMaskAlive = imoverlay2(PreviewNucMaskAlive, BarMask, [1 1 1]);
+    % imtool(PreviewNucMaskAlive)
+
+    PreviewIBA1 = imoverlay2(imadjust(max(ch1,[],3),[0 0.02]), bwperim(max(IBA1Mask,[],3)), [0 1 0]);
+    PreviewIBA1 = imoverlay2(PreviewIBA1, BarMask, [1 1 1]);
+    %imtool(PreviewIBA1)
+    
+    PreviewFOXA2 = imoverlay2(imadjust(max(ch4,[],3),[0 0.01]), bwperim(max(FOXA2Mask,[],3)), [1 0 0]);
+    PreviewFOXA2 = imoverlay2(PreviewFOXA2, BarMask, [1 1 1]);
+    %imtool(PreviewFOXA2)
+    
+    PreviewMAP2 = imoverlay2(imadjust(max(ch2, [], 3), [0 0.005]), bwperim(max(MAP2Mask,[],3)), [0 0 1]);
+    PreviewMAP2 = imoverlay2(PreviewMAP2, BarMask, [1 1 1]);
+    %imtool( PreviewMAP2)
+    
+    IdentityString = [Label.AreaName{:}, '_Idx_', num2str(Label.Idx)];
+    imwrite(PreviewFOXA2, [PreviewPath, filesep, IdentityString, '_', 'FOXA2', '.png'])
+    imwrite(PreviewHoechst, [PreviewPath, filesep, IdentityString, '_', 'Hoechst', '.png'])
+    imwrite(PreviewIBA1, [PreviewPath, filesep, IdentityString, '_', 'IBA1', '.png'])
+    imwrite(PreviewMAP2, [PreviewPath, filesep, IdentityString, '_', 'MAP2Mask', '.png'])
+    imwrite(PreviewNucMaskAlive, [PreviewPath, filesep, IdentityString, '_', 'NucMaskAlive', '.png'])
+   
+    %% Feature extraction
+    
+     ObjectsThisOrganoid = table();
+    ObjectsThisOrganoid.LabelIdx = {Label.Idx};
+    ObjectsThisOrganoid.AreaName = {Label.AreaName};
+    ObjectsThisOrganoid.NucMaskSum = sum(NucleiMask(:));
+    ObjectsThisOrganoid.NucMaskAlive = sum(NucMaskAlive(:));
+    ObjectsThisOrganoid.NucMaskHigh = sum(NucMaskHigh(:));
+    ObjectsThisOrganoid.MAP2MaskSum = sum(MAP2Mask(:));
+    ObjectsThisOrganoid.MAP2ByNucAlive = sum(MAP2Mask(:)) / sum(NucMaskAlive(:));
+    ObjectsThisOrganoid.FOXA2MaskSum = sum(FOXA2Mask(:));
+    ObjectsThisOrganoid.FOXA2ByNucAlive = sum(FOXA2Mask(:)) / sum(NucMaskAlive(:));
+    ObjectsThisOrganoid.IBA1MaskSum = sum(IBA1Mask(:));
+    ObjectsThisOrganoid.IBA1ByNucAlive = sum(IBA1Mask(:)) / sum(NucMaskAlive(:));
+    ObjectsThisOrganoid.DeadCells = sum(NucMaskHigh(:))/sum(NucleiMask(:));
+    ObjectsThisOrganoid.LiveNucCellCount = sum(NucMaskAlive(:)/67.9);
+    ObjectsThisOrganoid.MAP2CellCount = sum(MAP2Mask(:)/404.3);
+    ObjectsThisOrganoid.MAP2ByLiveCells = sum(ObjectsThisOrganoid.MAP2CellCount(:))/ (ObjectsThisOrganoid.LiveNucCellCount(:));
+    ObjectsThisOrganoid.FOXA2CellCount = sum(FOXA2Mask(:)/67.9);
+    ObjectsThisOrganoid.FOXA2ByLiveCells = sum(ObjectsThisOrganoid.FOXA2CellCount(:))/ (ObjectsThisOrganoid.LiveNucCellCount(:));
+    ObjectsThisOrganoid.IBA1CellCount = sum(IBA1Mask(:)/146.6);
+    ObjectsThisOrganoid.IBA1ByLiveCells = sum(ObjectsThisOrganoid.IBA1CellCount(:))/ (ObjectsThisOrganoid.LiveNucCellCount(:));
+   
+
+end
+
